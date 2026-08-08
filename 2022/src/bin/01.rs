93fb49c326c51060b7c1d0401be94526f5e0c98e
Connor McCarthy <connor15mcc@gmail.com>
2026-08-08 19:50:01 -0400

    day 1 part 1

--- 2022/src/bin/01.rs
+++ 2022/src/bin/01.rs
@@ -4,8 +4,22 @@ pub fn main() {
     println!("{}", result.unwrap())
 }
 
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut most_calories, mut current_cals) = (0, 0);
+    for line in input.lines() {
+        let line = line.trim();
+
+        if line == "" {
+            most_calories = most_calories.max(current_cals);
+            current_cals = 0;
+            continue;
+        }
+
+        let calories = line.parse::<u32>().expect("calories are ints");
+        current_cals += calories;
+    }
+
+    Some(most_calories)
 }
 
 #[cfg(test)]
@@ -15,8 +29,22 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = r#"
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
         "#;
         let result = part_one(input);
-        assert_eq!(result, None);
+        assert_eq!(result, Some(24_000));
     }
 }
