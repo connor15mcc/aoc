abf1111d2ea4a99fb4545614d5d8839e51235a56
Connor McCarthy <connor15mcc@gmail.com>
2026-08-10 22:06:01 -0400

    day 3 part 2

--- 2022/src/bin/03.rs
+++ 2022/src/bin/03.rs
@@ -4,10 +4,15 @@ pub fn main() {
     let input = include_str!("../../day03_part1.txt");
     let result = part_one(input);
     println!("Part 1: {}", result.unwrap());
+
+    let input = include_str!("../../day03_part2.txt");
+    let result = part_two(input);
+    println!("Part 2: {}", result.unwrap());
 }
 
 #[derive(Debug)]
 struct Rucksack {
+    items: u64,
     compartment_priority_masks: (u64, u64),
 }
 
@@ -15,6 +20,26 @@ impl Rucksack {
     fn shared_priority(self) -> u32 {
         let shared_mask = self.compartment_priority_masks.0 & self.compartment_priority_masks.1;
         shared_mask.trailing_zeros() + 1
+    }
+
+    fn badge(group: &[Self]) -> Result<Item, String> {
+        let [first, second, third] = group else {
+            return Err(format!(
+                "for safety, Elves must be in groups of 3 (not {})",
+                group.len()
+            ));
+        };
+
+        let badge = (first.items) & (second.items) & (third.items);
+        let count = badge.count_ones();
+        if count != 1 {
+            return Err(format!(
+                "Elven groups must share exactly one badge, found {}",
+                count
+            ));
+        }
+
+        Item::from_priority(badge.trailing_zeros() + 1)
     }
 }
 
@@ -30,9 +55,12 @@ impl FromStr for Rucksack {
                 mask | (1_u64 << bit)
             })
         };
+        let left = to_mask(left);
+        let right = to_mask(right);
 
         Ok(Self {
-            compartment_priority_masks: (to_mask(left), to_mask(right)),
+            items: left | right,
+            compartment_priority_masks: (left, right),
         })
     }
 }
@@ -40,6 +68,14 @@ impl FromStr for Rucksack {
 struct Item(char);
 
 impl Item {
+    fn from_priority(priority: u32) -> Result<Self, String> {
+        match priority {
+            1..=26 => Ok(Self(char::from_u32('a' as u32 + priority - 1).unwrap())),
+            27..=52 => Ok(Self(char::from_u32('A' as u32 + priority - 27).unwrap())),
+            _ => Err(format!("invalid item priority: {priority}")),
+        }
+    }
+
     fn priority(self) -> u32 {
         match self.0 {
             c @ 'a'..='z' => c as u32 - 'a' as u32 + 1,
@@ -63,6 +99,27 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
+pub fn part_two(input: &str) -> Option<u32> {
+    let rucksacks: Vec<_> = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let line = line.trim();
+            Rucksack::from_str(line).expect("valid rucksacks on each line")
+        })
+        .collect();
+
+    let sum = rucksacks
+        .chunks_exact(3)
+        .map(|group| {
+            let badge = Rucksack::badge(group).expect("groups share a badge");
+            badge.priority()
+        })
+        .sum();
+
+    Some(sum)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -79,5 +136,19 @@ mod tests {
         "#;
         let result = part_one(input);
         assert_eq!(result, Some(157));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = r#"
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        "#;
+        let result = part_two(input);
+        assert_eq!(result, Some(70));
     }
 }
