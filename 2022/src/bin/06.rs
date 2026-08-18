1c8853813f01a06ad142c5406ec02763d4f00961
Connor McCarthy <connor15mcc@gmail.com>
2026-08-18 12:21:35 -0400

    day 6 part 2

--- 2022/src/bin/06.rs
+++ 2022/src/bin/06.rs
@@ -4,14 +4,21 @@ pub fn main() {
     let input = include_str!("../../day06_part1.txt");
     let result = part_one(input);
     println!("Part 1: {}", result.unwrap());
+
+    let input = include_str!("../../day06_part2.txt");
+    let result = part_two(input);
+    println!("Part 2: {}", result.unwrap());
 }
 
 struct Datastream(String);
 
-impl Datastream {
-    const MARKER_LEN: usize = 4;
+struct MarkerLen(usize);
 
-    fn start_of_packet(&self) -> Option<usize> {
+impl Datastream {
+    const START_OF_PACKET: MarkerLen = MarkerLen(4);
+    const START_OF_MESSAGE: MarkerLen = MarkerLen(14);
+
+    fn start_pos(&self, MarkerLen(len): MarkerLen) -> Option<usize> {
         let is_unique = |window: &[char]| {
             window
                 .iter()
@@ -22,9 +29,9 @@ impl Datastream {
         self.0
             .chars()
             .collect::<Vec<_>>()
-            .windows(Self::MARKER_LEN)
+            .windows(len)
             .position(is_unique)
-            .map(|start_idx| start_idx + Self::MARKER_LEN)
+            .map(|start_idx| start_idx + len)
     }
 }
 
@@ -42,7 +49,16 @@ pub fn part_one(input: &str) -> Option<usize> {
         .expect("valid datastream");
     assert_eq!(lines.next(), None, "datastream must be exactly one line");
 
-    datastream.start_of_packet()
+    datastream.start_pos(Datastream::START_OF_PACKET)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut lines = input.trim().lines();
+    let datastream = Datastream::from_str(lines.next().expect("one line of a datastream"))
+        .expect("valid datastream");
+    assert_eq!(lines.next(), None, "datastream must be exactly one line");
+
+    datastream.start_pos(Datastream::START_OF_MESSAGE)
 }
 
 #[cfg(test)]
@@ -61,6 +77,21 @@ mod tests {
 
         for (input, expected) in cases {
             assert_eq!(part_one(input), Some(expected), "input: {input}");
+        }
+    }
+
+    #[test]
+    fn test_part_two() {
+        let cases = [
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+        ];
+
+        for (input, expected) in cases {
+            assert_eq!(part_two(input), Some(expected), "input: {input}");
         }
     }
 }
